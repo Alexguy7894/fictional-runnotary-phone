94a52ec9306db666c18f6ef855d0e07fdaea2236
Alex <99362675+Alexguy7894@users.noreply.github.com>
2026-05-22 19:38:27 -0700

    .

--- subway-surfers/loaders/v3/master-loader.js
+++ subway-surfers/loaders/v3/master-loader.js
@@ -16,14 +16,14 @@ if (!window.config.unityWebglLoaderUrl) {
         minor = versionSplit[1];
     switch (year) {
         case "2019":
-            window.config.unityWebglLoaderUrl = 1 === minor ? "./loaders/v2/unity/static/UnityLoader.2019.1.js" : "./loaders/v2/unity/static/UnityLoader.2019.2.js";
+            window.config.unityWebglLoaderUrl = 1 === minor ? "./loaders/v2/unity/static/UnityLoader.2019.1.js" : "https://github.com/Alexguy7894/fictional-runnotary-phone/blob/main/subway-surfers/loaders/v2/unity/static/UnityLoader.2019.2.js";
             break;
         default:
             window.config.unityWebglLoaderUrl = "./loaders/v2/unity/static/UnityLoader.js"
     }
 }
 var sdkScript = document.createElement("script");
-sdkScript.src = "./poki-sdk.js", sdkScript.onload = function() {
+sdkScript.src = "https://cdn.jsdelivr.net/gh/Alexguy7894/fictional-runnotary-phone@main/subway-surfers/poki-sdk.js", sdkScript.onload = function() {
     var i = document.createElement("script");
     i.src = root + loader, document.body.appendChild(i)
 }, document.body.appendChild(sdkScript);
